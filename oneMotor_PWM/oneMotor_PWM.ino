76c0dfc840c66987cf4f8acf27bd2910849a4b80
kuvasu <kuvasu@users.noreply.github.com>
2026-02-19 07:29:11 +0700

    Add Single Motor Control using PWM

--- oneMotor_PWM/oneMotor_PWM.ino
+++ oneMotor_PWM/oneMotor_PWM.ino
@@ -1,6 +1,6 @@
 //******************************************************
 // ROBOT SERIES
-// Simple Motor Control using PWM
+// Single Motor Control using PWM
 // Vasu Udompetaikul   Dept. of Agri Engineering, KMITL
 //******************************************************
 #define M1A  5  // Pin 5 <--> IN1  (Use IN1 & IN2 to control motor M1)
@@ -12,19 +12,27 @@ void setup() {
 }
 
 void loop() {
-  analogWrite(M1A, HIGH);  // Forward Rotation
+  analogWrite(M1A, 0);  // Forward 0% (Stop)
   digitalWrite(M1B, LOW);
   delay(3000);
 
-  digitalWrite(M1A, LOW);  // Stop
+  analogWrite(M1A, 128);  // Forward 50%
   digitalWrite(M1B, LOW);
-  delay(1000);
-
-  digitalWrite(M1A, LOW);   // Reverse Rotation
-  digitalWrite(M1B, HIGH);
   delay(3000);
 
-  digitalWrite(M1A, LOW);  // Stop
+  analogWrite(M1A, 255);  // Forward 100%
   digitalWrite(M1B, LOW);
-  delay(1000);
+  delay(3000);
+
+  digitalWrite(M1A, LOW);   // Reverse 0% (Stop)
+  analogWrite(M1B, 0);  
+  delay(3000);
+
+  digitalWrite(M1A, LOW);   // Reverse 50%
+  analogWrite(M1B, 128);  
+  delay(3000);
+
+  digitalWrite(M1A, LOW);   // Reverse 100%
+  analogWrite(M1B, 255);  
+  delay(3000);
 }
