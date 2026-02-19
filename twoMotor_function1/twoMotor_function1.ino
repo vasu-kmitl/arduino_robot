ff6be8aba1bb6c4a9fa5db4182bae18bbf0b6b05
kuvasu <kuvasu@users.noreply.github.com>
2026-02-19 08:01:54 +0700

    add 2 motors with functions

--- twoMotor_function1/twoMotor_function1.ino
+++ twoMotor_function1/twoMotor_function1.ino
@@ -1,6 +1,6 @@
 //******************************************************
 // ROBOT SERIES
-// Basic 2-Motor Control
+// Basic 2-Motor Control using Functions
 // Vasu Udompetaikul   Dept. of Agri Engineering, KMITL
 //******************************************************
 #define M1A  5   // Pin 5  <--> IN1  (Use IN1 & IN2 to control motor M1)
@@ -15,52 +15,51 @@ void setup() {
   pinMode(M2B, OUTPUT);
 }
 
+void Stop(){
+  digitalWrite(M1A, LOW);  // Stop
+  digitalWrite(M1B, LOW);
+  digitalWrite(M2A, LOW); 
+  digitalWrite(M2B, LOW);
+}
+
+void Forward(){
+  digitalWrite(M1A, HIGH);
+  digitalWrite(M1B, LOW);
+  digitalWrite(M2A, HIGH);
+  digitalWrite(M2B, LOW);
+}
+
+void Reverse(){
+  digitalWrite(M1A, LOW);
+  digitalWrite(M1B, HIGH);
+  digitalWrite(M2A, LOW);
+  digitalWrite(M2B, HIGH);
+}
+
+void Left(){
+  digitalWrite(M1A, LOW);
+  digitalWrite(M1B, HIGH);
+  digitalWrite(M2A, HIGH);
+  digitalWrite(M2B, LOW);
+}
+
+void Right(){
+  digitalWrite(M1A, HIGH);
+  digitalWrite(M1B, LOW);
+  digitalWrite(M2A, LOW);
+  digitalWrite(M2B, HIGH);
+}
+
 void loop() {
-  digitalWrite(M1A, HIGH);  // Forward Move
-  digitalWrite(M1B, LOW);
-  digitalWrite(M2A, HIGH);
-  digitalWrite(M2B, LOW);
-  delay(3000);
+  Forward();  delay(3000);
+  Stop();     delay(1000);
 
-  digitalWrite(M1A, LOW);  // Stop
-  digitalWrite(M1B, LOW);
-  digitalWrite(M2A, LOW); 
-  digitalWrite(M2B, LOW);
-  delay(1000);
+  Reverse();  delay(3000);
+  Stop();     delay(1000);
 
-  digitalWrite(M1A, LOW);   // Reverse Move
-  digitalWrite(M1B, HIGH);
-  digitalWrite(M2A, LOW);
-  digitalWrite(M2B, HIGH);
-  delay(3000);
+  Left();     delay(3000);
+  Stop();     delay(1000);
 
-  digitalWrite(M1A, LOW);  // Stop
-  digitalWrite(M1B, LOW);
-  digitalWrite(M2A, LOW); 
-  digitalWrite(M2B, LOW);
-  delay(1000);
-
-  digitalWrite(M1A, LOW);   // Turn Left
-  digitalWrite(M1B, HIGH);
-  digitalWrite(M2A, HIGH);
-  digitalWrite(M2B, LOW);
-  delay(3000);
-
-  digitalWrite(M1A, LOW);  // Stop
-  digitalWrite(M1B, LOW);
-  digitalWrite(M2A, LOW); 
-  digitalWrite(M2B, LOW);
-  delay(1000);
-
-  digitalWrite(M1A, HIGH);  // Turn Right
-  digitalWrite(M1B, LOW);
-  digitalWrite(M2A, LOW);
-  digitalWrite(M2B, HIGH);
-  delay(3000);
-
-  digitalWrite(M1A, LOW);  // Stop
-  digitalWrite(M1B, LOW);
-  digitalWrite(M2A, LOW); 
-  digitalWrite(M2B, LOW);
-  delay(1000);
+  Right();    delay(3000);
+  Stop();     delay(1000);
 }
---
+++ twoMotor_function2/twoMotor_function2.ino
@@ -0,0 +1,59 @@
+//******************************************************
+// ROBOT SERIES
+// Basic 2-Motor Control using Functions #2
+// Vasu Udompetaikul   Dept. of Agri Engineering, KMITL
+//******************************************************
+#define M1A  5   // Pin 5  <--> IN1  (Use IN1 & IN2 to control motor M1)
+#define M1B  6   // Pin 6  <--> IN2
+#define M2A  9   // Pin 9  <--> IN3  (Use IN3 & IN4 to control motor M2)
+#define M2B  10  // Pin 10 <--> IN4
+
+void setup() {
+  pinMode(M1A, OUTPUT);
+  pinMode(M1B, OUTPUT);
+  pinMode(M2A, OUTPUT);
+  pinMode(M2B, OUTPUT);
+}
+
+void move(char dir) {
+  switch(dir){
+    case 'S': digitalWrite(M1A, LOW);
+              digitalWrite(M1B, LOW);
+              digitalWrite(M2A, LOW); 
+              digitalWrite(M2B, LOW);
+              break;
+    case 'F': digitalWrite(M1A, HIGH);
+              digitalWrite(M1B, LOW);
+              digitalWrite(M2A, HIGH);
+              digitalWrite(M2B, LOW);
+              break;
+    case 'B': digitalWrite(M1A, LOW);
+              digitalWrite(M1B, HIGH);
+              digitalWrite(M2A, LOW);
+              digitalWrite(M2B, HIGH);
+              break;
+    case 'L': digitalWrite(M1A, LOW);
+              digitalWrite(M1B, HIGH);
+              digitalWrite(M2A, HIGH);
+              digitalWrite(M2B, LOW);
+              break;
+    case 'R': digitalWrite(M1A, HIGH);
+              digitalWrite(M1B, LOW);
+              digitalWrite(M2A, LOW);
+              digitalWrite(M2B, HIGH);
+  }
+}
+
+void loop() {
+  move('F');  delay(3000);
+  move('S');  delay(1000);
+
+  move('B');  delay(3000);
+  move('S');  delay(1000);
+
+  move('L');  delay(3000);
+  move('S');  delay(1000);
+
+  move('R');  delay(3000);
+  move('S');  delay(1000);
+}
